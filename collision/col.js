39b0371b75082cae10ece5659f92d0f33513f012
Shane <shanehoban@gmail.com>
2015-02-04 22:58:53 +0000

    Updated a lot. Structure, how the animates work etc.
    Need to implement a way to include acceleration based off ideas from 2010: http://stackoverflow.com/a/4193729/1173155

--- collision/col.js
+++ collision/col.js
@@ -1,95 +1,111 @@
-$(document).ready(function(){
 
-	var Game = {
-				game: $('#colGame'),
-				gravity: 100
-				};
-		Game.width = Game.game.width();
-		Game.height = Game.game.height();
-	var game = Game.game;
-	
-	
-	function renderPlayer(){
-		Player.player.css({
-			top: Player.location.top,
-			bottom: Player.location.bottom,
-			left: Player.location.left,
-			right: Player.location.right
-		});
-	}
+var Game = {
+		game: $('.col-game'),
+		gravity: 800,
+		friction: 100
+	};
+	Game.width = Game.game.width();
+	Game.height = Game.game.height();
 
+	Game.jumpHeight = ((Game.gravity/25) * 1.25);
+	Game.moveDist = (100/10);
+
+	Game.jumpAni = (Game.gravity * 0.3);
+	Game.fallAni = (Game.gravity * 0.3);
+
+var game = Game.game;
+	game.focus();
+x
+
+var Player = {
+		actions: {
+			jump: jump,
+			moveRight: moveRight,
+			moveLeft: moveLeft,
+			},
+		isJumping: false,
+		isFalling: false,
+		isMovingRight: false,
+		isMovingLeft: false
+	};
+
+var p = $('.col-player');
+
+var x = 0;
 
 	/////////// Movements /////////////
-	
-			var moveLeft = function(){
-				$('#debugLastAction').text('moveLeft');
-				
-				};
-				
-			var jump = function(){
-				$('#debugLastAction').text('jump');
-					Player.location.bottom = Player.location.bottom + 20;
-					renderPlayer();
-				};
-				
-			var moveRight = function(){
-				$('#debugLastAction').text('moveRight');
-				
-				};
-				
-			var fall = function(){
-				$('#debugLastAction').text('fall');
-				
-				};
-	
-	
-	////////// End Movements //////////
-	
-	var Player = {
-			player: $('#colPlayer'),
-			actions: {
-				moveLeft: moveLeft,
-				jump: jump,
-				moveRight: moveRight,
-				fall: fall
-				},
-		};
-		Player.location = {
-				top: Player.player.offset().top,
-				bottom: Player.player.offset().bottom,
-				left: Player.player.offset().left,
-				right:  Player.player.offset().right,
+
+	function jump(){
+		if(!Player.isJumping && !Player.isFalling){
+			Player.isJumping = true;
+				p.animate({bottom: "+=" + Game.jumpHeight}, Game.jumpAni, function(){
+					Player.isJumping = false;
+					Player.isFalling = true;
+						p.animate({bottom: "-="+ Game.jumpHeight}, Game.fallAni, function(){
+							setTimeout(function(){
+								Player.isFalling = false;
+								}, 100);
+						});
+				});
+			renderDebug("jump");
 		}
+	}
+
+	function moveRight(){
+		p.animate({left: "+=" + Game.moveDist}, {duration: Game.moveAni, queue: false}, function(){
+		});
+		renderDebug("moveRight");
+	}
+
+	function moveLeft(){
+		p.animate({left: "-=" + Game.moveDist}, {duration: Game.moveAni, queue: false}, function(){
+		});
+		renderDebug("moveLeft");
+	}
+
+	////////// End Movements //////////
+
+
 
 	game.keydown(function(){
-		$( "#debugKey" ).text(event.which);
+		x++;
+		$( ".debug-key" ).text(event.which);
 		switch(event.which){
 			case 37:
-				Player.actions.moveLeft();
-				break;
-			case 38:
-				Player.actions.jump();
-				break;
+			 	Player.actions.moveLeft();
+			 	break;
+		 	case 38:
+		 		Player.actions.jump();
+		 		break;
 			case 39:
-				Player.actions.moveRight();
-				break;
-			case 40:
-				Player.actions.fall();
-				break;
+			 	Player.actions.moveRight();
+			 	break;
+			// case 40:
+			// 	Player.actions.fall();
+			// 	break;
 		}
 	});
-		
-	
-	/////////// Debug /////////////////
-	
-	$('#debugHeight').text(Game.height);
-	$('#debugWidth').text(Game.width);
-	
-	$('#playerTop').text(Player.location.top);
-	$('#playerBottom').text(Player.location.bottom);
-	$('#playerLeft').text(Player.location.left);
-	$('#playerRight').text(Player.location.right);
-	
-	
 
-});
+	game.keyup(function(){
+		x=0;
+		renderDebug();
+	});
+
+
+	/////////// Debug /////////////////
+
+	function renderDebug(action){
+
+		$('.debug-height').text(Game.height);
+		$('.debug-width').text(Game.width);
+
+		$('.debug-button-down-count').text(x);
+
+		$('.debug-last-action').text(action);
+
+		$('.debug-player-top').text(p.offset().top);
+	//	$('.debug-player-bottom').text(p.offset().top);
+	//	$('.debug-player-left').text(p.offset().top);
+	//	$('.debug-player-right').text(p.offset().top);
+	}
+	renderDebug();
