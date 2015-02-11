d73e187c502d669f04a80e07685081d60e6e0360
Hoban, Shane <Shane.Hoban@fmr.com>
2015-02-11 13:10:37 +0000

    Added player image Added background image Added viewport Enabled right movement to coincide with viewport using margin
    - Need to:
     Stop margin when reached end
     Make margin move back when going left

--- collision/col.js
+++ collision/col.js
@@ -1,15 +1,20 @@
 var Game = {
 		game: $('.col-game'),
+		viewport: $('.col-viewport'),
 		gravity: 1,
 		friction: 1,
-		startSpeed: 0.02,
-		maxSpeed: 0.5,
-		acceleration: 0.01,
-		deceleration: 0.01,
+		startSpeed: 1,
+		maxSpeed: 5,
+		acceleration: 0.5,
+		deceleration: 0.5,
 		reDrawMs: 10
 	};
 	Game.width = Game.game.width();
 	Game.height = Game.game.height();
+	Game.viewport.width = Game.viewport.width();
+	Game.viewport.height = Game.viewport.height();
+	Game.viewport.move = (Game.viewport.width/100) * 70;
+	Game.viewport.left = 0;
 
 var game = Game.game;
 	game.focus(); // so don't have to click div
@@ -25,10 +30,7 @@ var Player = {
 
 var p = $('.col-player');
 
-
-Game.recalc = function(){
-	Game.moveDist = (Game.width/100)*Game.startSpeed; // 0.02% (acceleration also comes into play)
-	
+Game.recalc = function(){	
 	Game.maxJumpHeight = (Game.height/100)*(Game.gravity*10);
 	Game.jumpAcceleration = ((Game.maxJumpHeight/100)*5)/Game.gravity;
 };
@@ -86,11 +88,21 @@ Game.recalc = function(){
 			Player.currentSpeed += (Player.currentSpeed === 0) ? Game.startSpeed : 0;
 			Player.currentSpeed = (Player.currentSpeed+(Game.acceleration/Game.friction)) ;
 			Player.currentSpeed = (Player.currentSpeed >= Game.maxSpeed) ? Game.maxSpeed : Player.currentSpeed;
-			Game.moveDist = (Game.width/100) * Player.currentSpeed;
-			p.css("left", "+=" + Game.moveDist);
-				setTimeout(function(){ 
-					Player.moveRight(); 
-				}, Game.reDrawMs);
+
+			if(p.position().left >= Game.viewport.move){
+				Game.game.css("margin-left", "-=" + Player.currentSpeed);
+				Game.viewport.left += Player.currentSpeed;
+					setTimeout(function(){ 
+						Player.moveRight(); 
+					}, Game.reDrawMs);
+			}
+			else{
+			 	p.css("left", "+=" + Player.currentSpeed);
+					setTimeout(function(){ 
+			 			Player.moveRight(); 
+			 		}, Game.reDrawMs);
+			}
+		
 		}
 		renderDebug("moveRight");
 	};
@@ -99,12 +111,16 @@ Game.recalc = function(){
 	Player.moveLeft = function(){
 		Game.recalc();
 
+		if(p.position().left <= 0){
+			p.css("left", 0);
+			return;
+		}
+
 		if(Player.isMovingLeft){
 			Player.currentSpeed += (Player.currentSpeed === 0) ? Game.startSpeed : 0;
 			Player.currentSpeed = (Player.currentSpeed+(Game.acceleration/Game.friction)) ;
 			Player.currentSpeed = (Player.currentSpeed >= Game.maxSpeed) ? Game.maxSpeed : Player.currentSpeed;
-			Game.moveDist = (Game.width/100) * Player.currentSpeed;
-			p.css("left", "-=" + Game.moveDist);
+			p.css("left", "-=" + Player.currentSpeed);
 				setTimeout(function(){
 					Player.moveLeft(); 
 				}, Game.reDrawMs);
@@ -180,8 +196,13 @@ Game.recalc = function(){
 
 	function renderDebug(action){
 
-		$('.debug-height').text(Game.height);
-		$('.debug-width').text(Game.width);
+		$('.debug-game-height').text(Game.height);
+		$('.debug-game-width').text(Game.width);
+
+		$('.debug-viewport-height').text(Game.viewport.height);
+		$('.debug-viewport-width').text(Game.viewport.width);
+		$('.debug-viewport-left').text(Game.viewport.left);
+
 		$('.debug-button-down-count').text(x);
 		$('.debug-last-action').text(action);
 		$('.debug-speed').text(Math.round(Player.currentSpeed*100)/100);
@@ -190,5 +211,6 @@ Game.recalc = function(){
 
 		$('.debug-player-top').text(p.position().top);
 		$('.debug-player-left').text(p.position().left);
+		$('.debug-player-right').text(p.position().right);
 	}
 	renderDebug();
